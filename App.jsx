7925aa38d09565db2222094a386b72080b1712dd
Alex Ejiga <78400957+XauDe@users.noreply.github.com>
2024-06-13 22:33:59 +0100

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -23,7 +23,7 @@ function App() {
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
-          Just making sure my git is set up properly
+          Just making sure my git is set up properly hmm
         </p>
       </div>
       <p className="read-the-docs">
